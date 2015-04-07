ab3a20b92117e24ddbb0020f6b94d459c18225e5
Chandrashekar Tippur <ctippur@gmail.com>
2015-04-06 21:01:55 -0700

    Adding helper.rb

--- test/plugin/test_in_appdynamics.rb
+++ test/plugin/test_in_appdynamics.rb
@@ -1,34 +1,33 @@
 require 'helper'
 
-class appdynamicsTrapInputTest < Test::Unit::TestCase
+class AppdynamicsInputTest < Test::Unit::TestCase
+
   def setup
     Fluent::Test.setup
   end
-  
-  # Add config here
+
   CONFIG = %[
-    # Example hostt 0
-    # Example port 1062
-    # Example tag alert.snmptrap
-    tag alert.appdynamics.raw
+    username test_username
+    password test_password
+    endpoint test.endpoint.com
   ]
 
-  def create_driver(conf=CONFIG)
-    Fluent::Test::InputTestDriver.new(Fluent::appdynamicsTrapInput, tag='test_tag').configure(conf)
+  def create_driver(conf = CONFIG)
+    Fluent::Test::InputTestDriver.new(Fluent::AppdynamicsInput).configure(conf)
   end
 
-  # Configure the test
   def test_configure
-    d = create_driver('')
-    assert_equal "pleasechangeme.com", d.instance.endpoint
-    assert_equal "username", d.instance.user
-    assert_equal "password", d.instance.pass
-    assert_equal "account", d.instance.account
-    assert_equal "300".to_i, d.instance.interval
-    assert_equal "false", d.instance.include_raw
-    assert_equal 'alert.appdynamics.raw', d.instance.tag
-    # Example assert_equal "0".to_i, d.instance.host
-    # Example assert_equal "1062".to_i, d.instance.port
-    # Example assert_equal 'alert.snmptrap', d.instance.tag
+    assert_nothing_raised { create_driver }
   end
+
+  def test_params
+    d = create_driver
+    assert_equal "test.endpoint.com", d.instance.endpoint
+    assert_equal "test_username", d.instance.username
+    assert_equal "test_password", d.instance.password
+    assert_equal "10".to_i, d.instance.interval
+    assert_equal "false", d.instance.include_raw
+    assert_equal 'alert.appdynamics', d.instance.tag
+  end
+
 end
